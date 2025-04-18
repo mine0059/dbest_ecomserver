c5ca41ecdf1bc9335e46c7fea513e1b27d6e9260
Emmanuel <onojefemuemine@gmail.com>
2025-04-18 16:25:46 +0100

    Text commit for mine0059

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
-require('dotenv').config(); // Load environment variables from .env file
+const cors = require('cors');
+require('dotenv/config')
 
 const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World!',
+    });
+});
+
 const env = process.env;
 
 const hostname = env.HOST;
 const port = env.PORT;
 
-console.info('Infomation:', hostname);
-
 app.listen(port, hostname, () => {
-  console.log(`Server is running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
