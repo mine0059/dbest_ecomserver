418697dfaae076de02b162a9916c5037bd96b13b
Emmanuel <onojefemuemine@gmail.com>
2025-06-01 13:52:52 +0100

    modified app.js file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const authJwt = require('./src/middlewares/jwt');
+const errorHandler = require('./src/middlewares/error_handler');
 
 const app = express();
 const env = process.env;
@@ -13,9 +15,25 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cors());
 // app.options('*', cors());
+app.use(authJwt());
+app.use(errorHandler);
 
 const authRouter = require('./src/routes/auth.route');
+const usersRouter = require('./src/routes/users.route');
+const adminRouter = require('./src/routes/admin.route');
+const categoryRouter = require('./src/routes/categories.route');
+
+app.use((req, res, next) => {
+    console.log('Request URL:', req.originalUrl);
+    console.log('Request Method:', req.method);
+    next();
+});
+
 app.use(`${API}/`, authRouter);
+app.use(`${API}/users`, usersRouter);
+app.use(`${API}/admin`, adminRouter);
+app.use(`${API}/category`, categoryRouter);
+app.use('/public', express.static(__dirname + '/public'));
 
 const hostname = env.HOST;
 const port = env.PORT;
@@ -26,6 +44,8 @@ if (!dbUri) {
     console.error('MongoDB connection string not defined in environment variables');
     process.exit(1);
 }
+
+require('./src/helpers/cron_job');
 
 // MongoDB connection
 mongoose.connect(dbUri).then(() => {
